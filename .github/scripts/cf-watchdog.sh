d341fa4b2f403a72357afbd972e49cdd3334b61b
hhquan888 <hhquan888@gmail.com>
2026-08-22 19:59:23 +0700

    Delete .github/scripts directory

--- .github/scripts/cf-watchdog.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Watchdog: automatically restarts cloudflared tunnel if it dies
-while true; do
-  sleep 15
-  pgrep -f 'cloudflared tunnel --url http://localhost:8000' > /dev/null || \
-    nohup cloudflared tunnel --url http://localhost:8000 >> cloudflared.log 2>&1 &
-  pgrep -f 'cloudflared tunnel --url http://localhost:3000' > /dev/null || \
-    nohup cloudflared tunnel --url http://localhost:3000 >> cloudflared-upload.log 2>&1 &
-done
